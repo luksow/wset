7886ee40d23b4c56128a3f07951de7d274fc5fe7
Łukasz Sowa <contact@luksow.com>
2023-07-31 19:42:06 +0200

    Fixing some build errors.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,46 @@ import { Title, Container, Table, Anchor, Badge, ScrollArea, Divider, Text } fro
 import * as _ from 'lodash';
 import grapesRaw from "./grapes.json";
 
+type GrapeRaw = {
+  order: number,
+  name: string,
+  climate: string,
+  acidity: string;
+  tannins: string;
+  sweetness: string;
+  body: string;
+  flavour: string;
+  oak: string;
+  additional_characteristics: string;
+  aging: string;
+  country: string;
+  region: string;
+  characteristics: string;
+}
+
+type Climate = "Cool" | "Moderate" | "Warm";
+type Acidity = "Low" | "Medium" | "High";
+type Tannins = "Low" | "Medium" | "High";
+type Sweetness = "Dry" | "Off-dry" | "Medium" | "Sweet";
+type WineBody = "Light" | "Medium" | "Full";
+
+type GrapeWithRegion = {
+  order: number,
+  name: string,
+  climate: Climate[],
+  acidity: Acidity[],
+  tannins: Tannins[],
+  sweetness: Sweetness[],
+  body: WineBody[],
+  flavour: string[],
+  oak: string,
+  aging: string[],
+  additionalCharacteristics: string[],
+  country: string,
+  region: string,
+  regionalCharacteristics: string,
+}
+
 export default function IndexPage() {
   const grapes: GrapeWithRegion[] = grapesRaw.map((grapeRaw) => {
     const grape: GrapeWithRegion = {
@@ -90,7 +130,6 @@ export default function IndexPage() {
 
   const generateSweetnessBadges = (sweetness: Sweetness[]) => {
     const color = (sweetness: Sweetness) => {
-      console.log(sweetness);
       switch (sweetness) {
         case "Dry":
           return "blue";
